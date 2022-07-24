f0d9f11a3cbeb21b1e3c54d8f3158e00173f0926
Max <zeetreex@gmail.com>
2022-07-24 21:20:22 +0300

    created registration/auth form

--- app/components/screens/auth/Auth.tsx
+++ app/components/screens/auth/Auth.tsx
@@ -11,66 +11,64 @@ import AuthFields from '@/screens/auth/AuthFields'
 import styles from './Auth.module.scss'
 
 const Auth: FC = () => {
-  useAuthRedirect()
+	useAuthRedirect()
 
-  const { isLoading } = useAuth()
+	const { isLoading } = useAuth()
 
-  const [type, setType] = useState<'login' | 'register'>('login')
+	const [type, setType] = useState<'login' | 'register'>('login')
 
-  const {
-    register: registerInput,
-    handleSubmit,
-    formState,
-    reset,
-  } = useForm<IAuthInput>({
-    mode: 'onChange',
-  })
+	const {
+		register: registerInput,
+		handleSubmit,
+		formState,
+		reset,
+	} = useForm<IAuthInput>({
+		mode: 'onChange',
+	})
 
-  const login = (data: any) => {
-  }
+	const login = (data: any) => alert('logindata')
 
-  const register = (data: any) => {
-  }
+	const register = (data: any) => alert('regdata')
 
-  const onSubmit: SubmitHandler<IAuthInput> = (data) => {
-    if (type === 'login') login(data)
-    else if (type === 'register') register(data)
+	const onSubmit: SubmitHandler<IAuthInput> = (data) => {
+		if (type === 'login') login(data)
+		else if (type === 'register') register(data)
 
-    reset()
-  }
+		reset()
+	}
 
-  return (
-    <Meta title='Auth'>
-      <section className={styles.wrapper}>
-        <form onSubmit={handleSubmit(onSubmit)}>
-          <Heading title='Auth' className='mb-6' />
+	return (
+		<Meta title="Auth">
+			<section className={styles.wrapper}>
+				<form onSubmit={handleSubmit(onSubmit)}>
+					<Heading title="Auth" className="mb-6" />
 
-          <AuthFields
-            register={registerInput}
-            formState={formState}
-            isPasswordRequired=
-          />
+					<AuthFields
+						register={registerInput}
+						formState={formState}
+						isPasswordRequired
+					/>
 
-          <div className={styles.buttons}>
-            <Button
-              type='submit'
-              onClick={() => setType('login')}
-              disabled={isLoading}
-            >
-              Login
-            </Button>
-            <Button
-              type='submit'
-              onClick={() => setType('register')}
-              disabled={isLoading}
-            >
-              Register
-            </Button>
-          </div>
-        </form>
-      </section>
-    </Meta>
-  )
+					<div className={styles.buttons}>
+						<Button
+							type="submit"
+							onClick={() => setType('login')}
+							disabled={isLoading}
+						>
+							Login
+						</Button>
+						<Button
+							type="submit"
+							onClick={() => setType('register')}
+							disabled={isLoading}
+						>
+							Register
+						</Button>
+					</div>
+				</form>
+			</section>
+		</Meta>
+	)
 }
 
 export default Auth
--- app/components/ui/form-elements/form.interface.ts
+++ app/components/ui/form-elements/form.interface.ts
@@ -1,11 +1,11 @@
 import { ButtonHTMLAttributes, InputHTMLAttributes } from 'react'
-import { FieldError } from 'react-hook-form'
+import { DeepMap, FieldError } from 'react-hook-form'
 
 export interface IButton extends ButtonHTMLAttributes<HTMLButtonElement> {}
 
 export interface IFieldProps {
 	placeholder: string
-	error?: any
+	error?: DeepMap<any, FieldError>
 }
 
 type TypeInputPropsField = InputHTMLAttributes<HTMLInputElement> & IFieldProps
